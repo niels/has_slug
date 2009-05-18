4adb8cd23078164c75871563d50dd0898fc124e6
Niels Ganser <niels@herimedia.com>
2009-05-18 23:30:50 +0200

    Provide some context for backtraces into class_eval

--- lib/has_slug.rb
+++ lib/has_slug.rb
@@ -12,9 +12,9 @@ module ActiveRecord #:nodoc:
       def self.included(base) #:nodoc:
         base.extend(ClassMethods)
       end
-      
+
       module ClassMethods
-        
+
         # has_slug takes an arbitrary number of Symbols as arguments. Those
         # should be methods (i.e. ActiveRecord attributes) of your model which
         # return strings to be slugalized.
@@ -22,7 +22,7 @@ module ActiveRecord #:nodoc:
         # or   has_slug(:category, :short_title)
         def has_slug(*parts)
           # Ouch, that's not pretty! Any beautification ideas?
-          self.class_eval <<-EOR
+          self.class_eval <<-EOR, __FILE__, __LINE__ + 1
             def to_param
               p = self.id.to_s
               #{parts.collect { |part| 'p += "-#{(self.' + part.to_s + ').parameterize}" unless self.' + part.to_s + '.blank?'+"\n"}}
